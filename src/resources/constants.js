5c028920ff46dc711ae99080c626cfe30f6f20e4
Isabel Bou <isabelbouaguiar@gmail.com>
2023-11-23 16:22:45 +0100

    feat: Parameterize card content, now defined in constants

--- src/resources/constants.js
+++ src/resources/constants.js
@@ -1,3 +1,11 @@
 export const scores = {
     JONICO: 'https://raw.githubusercontent.com/IsabelBou/MeiTestFiles/main/DoM.mei'
 }
+
+export const cards = {
+    TEMPLATE:{
+        title: 'Title',
+        description: 'More Information',
+        score: scores.JONICO,
+    }
+}
