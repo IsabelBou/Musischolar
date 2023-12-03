0e5fa37b1c79bd6d967fa9c8a24f25453cea3ce2
Isabel Bou <isabelbouaguiar@gmail.com>
2023-12-03 20:51:07 +0100

    feat: Play Base64-encoded MIDI string lifted from VerovioRenderer component

--- src/themes/components/MidiPlayer.jsx
+++ src/themes/components/MidiPlayer.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PlayPauseToggle from './PlayPauseToggle';
 import VerovioRenderer from './Verovio';
+import { JZZ } from 'jzz';
+import { SMF } from 'jzz-midi-smf';
+import { Tiny } from 'jzz-synth-tiny';
+
+async function play(midi) {
+    await SMF(JZZ);
+    await Tiny(JZZ);
+    await JZZ.synth.Tiny.register('Web Audio');
+    const midiout = await JZZ().openMidiOut('Web Audio');
+    const smf = new JZZ.MIDI.SMF(JZZ.lib.fromBase64(midi));
+    const player = smf.player();
+    player.connect(midiout);
+    player.play();
+}
 
 const Player = (props) => {
     const { score } = props;
     const [isPlaying, setIsPlaying] = useState(false);
     const [midi, setMidi] = useState();
+    useEffect(() => {
+        async function playMidi() {
+            if(midi) {
+                await play(midi)
+            }
+        }
+        playMidi();
+    }, [midi])
+
+    //TODO: Change button to pause when MIDI ends
+    //FIXME: Get the MIDI to always respond to button input
+    //OPTIMIZE: Reduce load time of midi in web
 
     return(
         <div>
             <VerovioRenderer url = { score } setMidi = {setMidi} />
-            <>{midi}</>
             <PlayPauseToggle onClick={() => setIsPlaying(!isPlaying)} isPlaying={isPlaying}/>
         </div>
     )
