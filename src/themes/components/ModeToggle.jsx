6d81b96c53312a4284e9d134184440fcc4789b83
Isabel Bou <isabelbouaguiar@gmail.com>
2023-12-06 13:36:55 +0100

    feat: MIDI plays on command, synced with Play/Pause toggle button. Also solves double initial rendering.

--- src/themes/components/ModeToggle.jsx
+++ src/themes/components/ModeToggle.jsx
@@ -1,23 +1,6 @@
 import {useColorMode, Box, Tooltip } from "@chakra-ui/react";
 import { PiSunDuotone, PiMoonStarsDuotone } from 'react-icons/pi'
 
-/* // 1. Import `extendTheme`
-import { extendTheme } from '@chakra-ui/react'
-
-// 2. Extend the theme with new layer styles
-const sunset = extendTheme({
-  layerStyles: {
-    _dark: {
-      backgroundPosition: 'top',
-      transition: '0.8s'
-    },
-    _light: {
-      backgroundPosition: 'bottom',
-      transition: '0.8s'
-    },
-  },
-})
- */
 const Toggle = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
@@ -32,10 +15,6 @@ const Toggle = () => {
           _focus={{  }}
           bgGradient = 'linear(to-b, #030033 0%, #611b87 35%, #5570cc 69%, #5abaf0 77%, #fff8e9 100%)'
           backgroundSize = "auto 300%"
-          _hover =
-            {{backgroundPosition: 'center',
-            transition: '0.5s'
-            }}
             //FIXME: Remove line appearing on ends of gradient
             //TODO: Add animations on Hover (transform scale) and on change (Sun dawning and Moon descending). Also maybe improve icon coloring selection.
           _dark={{
