015e0c1df2072d80612e7c503ab7761e32860dc5
Isabel Bou <isabelbouaguiar@gmail.com>
2023-12-11 11:20:24 +0100

    feat: Adds popover upon clicking Tempo Selector button

--- src/themes/components/TempoSelector.jsx
+++ src/themes/components/TempoSelector.jsx
@@ -1,16 +1,32 @@
-import { Button, Tooltip } from "@chakra-ui/react";
+import { Button, Tooltip} from "@chakra-ui/react";
 import { PiMetronomeDuotone } from "react-icons/pi";
-
+import { Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverArrow, PopoverBody } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react" //Needed for Popover workaround
 const TempoSelector = () => {
+
 //TODO: Slider for Tempo adjustment linked to midi playback
+
     return (
         <div>
-            <Tooltip label = 'Change Tempo' >
-                <Button aria-label = "Adjust Tempo" >
-                    <PiMetronomeDuotone />
-                </Button>
-            </Tooltip>
-
+            <Popover placement='left-start'>
+                <Tooltip label = 'Change Tempo' >
+                    <Box display="inline-block"> {/*Box is needed as workaround to keep button's tooltip, see here: https://github.com/chakra-ui/chakra-ui/issues/2843#issuecomment-748641805 */}
+                        <PopoverTrigger>
+                            <Button aria-label = "Adjust Tempo" >
+                                <PiMetronomeDuotone />
+                            </Button>
+                        </PopoverTrigger>
+                    </Box>
+                </Tooltip>
+                <PopoverContent>
+                    <PopoverHeader> Choose your tempo </PopoverHeader>
+                    <PopoverArrow />
+                    <PopoverBody>
+                        Slider will go here
+                    </PopoverBody>
+                </PopoverContent>
+            </Popover>
+        
         </div>
     );
 };
