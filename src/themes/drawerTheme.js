b55293c0c2b50acbd49ccc81a2ea0b74f67e9f1c
Isabel Bou <isabelbouaguiar@gmail.com>
2023-12-31 08:14:44 +0100

    feat: themed drawer

---
+++ src/themes/drawerTheme.js
@@ -0,0 +1,21 @@
+import { drawerAnatomy as parts } from "@chakra-ui/anatomy"
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system"
+
+const { definePartsStyle, defineMultiStyleConfig } =
+    createMultiStyleConfigHelpers(parts.keys)
+
+const baseStyle = definePartsStyle({
+    dialog: {
+        backgroundColor: "primary.75",
+        _dark:{
+            backgroundColor: "primary.780",
+        }
+    },
+    overlay: { 
+        backdropFilter: 'auto',
+        backdropBlur: '1.5px',
+        backdropSaturate:'50%',        
+    }
+})
+
+export const drawerTheme = defineMultiStyleConfig({ baseStyle })
