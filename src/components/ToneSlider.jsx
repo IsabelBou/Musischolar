d82c2dfb6bfb246cbb32c5c82b84b83006e7e559
Isabel Bou <isabelbouaguiar@gmail.com>
2024-01-08 12:55:44 +0100

    feat: visualize selected key signature information through slider

--- src/components/ToneSlider.jsx
+++ src/components/ToneSlider.jsx
@@ -1,25 +1,32 @@
 import { Slider, SliderTrack, SliderThumb, SliderMark } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react';
 import { useState } from 'react';
+import { KEY } from '../resources/constants';
 
 const ToneSlider = () => {
 
     const [sliderValue, setSliderValue] = useState(0)
 
     const labelStyles = {
-        ml: '-1',
+        marginLeft: '-1',
         marginTop: '2',
         fontSize: 'sm',
         textAlign: 'center',
     }
 
+    let symbol = (sliderValue) => {
+        if      (sliderValue < 0) { return '♭' }
+        else if (sliderValue > 0) { return '♯' }
+        else { return '♮' }
+    }
+
 return (
     <Box marginBottom = { 5 } marginTop = { 10 } >
-        <Slider defaultValue = { 0 } min = { -7 } max = { 7 } step = { 1 } aria-label='key-selection-slider' onChange={(val) => setSliderValue(val)}>
+        <Slider defaultValue = { 0 } min = { -6 } max = { 6 } step = { 1 } aria-label='key-selection-slider' onChange={(val) => setSliderValue(val)}>
             <SliderTrack bg = 'primary.500'/>
             <SliderMark value = { 0 } {...labelStyles}  >♮</SliderMark>
-            <SliderMark value = { -7 } {...labelStyles} >♭</SliderMark>
-            <SliderMark value = { 7 } {...labelStyles}  >♯</SliderMark>
+            <SliderMark value = { -6 } {...labelStyles} >♭</SliderMark>
+            <SliderMark value = { 6 } {...labelStyles}  >♯</SliderMark>
             <SliderTrack />
             <SliderThumb boxSize = { 3 } />
             <SliderMark
@@ -30,11 +37,13 @@ return (
                 mt = '-10'
                 ml = '-5'
                 w = '10' >
-                {Math.abs(sliderValue)}
+                {Math.abs(sliderValue)}{symbol(sliderValue)}
             </SliderMark>
         </Slider>
+        <Box mt='5'> Tonalidades - {Math.abs(sliderValue)}{symbol(sliderValue)} </Box>
+        <Box > Mayor: { KEY[sliderValue].mayor }</Box>
+        <Box> Menor: { KEY[sliderValue].menor }</Box>
     </Box>
-);
-};
+)};
 
 export default ToneSlider;
--- src/resources/constants.js
+++ src/resources/constants.js
@@ -14,9 +14,70 @@ export const cards = {
 
 // Key signature possibilities
 export const KEY = {
+    //Verovio transposition info: https://book.verovio.org/advanced-topics/transposition.html#transposition-by-tonic-pitch
     0: {
         mayor: 'Do',
         menor: 'La',
         trasposevrv: 'P1',
-    }
+    },
+    1: {
+        mayor: 'Sol',
+        menor: 'Mi',
+        trasposevrv: '+G',
+    },
+    2: {
+        mayor: 'Re',
+        menor: 'Si',
+        trasposevrv: 'D',
+    },
+    3: {
+        mayor: 'La',
+        menor: 'Fa♯',
+        trasposevrv: 'A',
+    },
+    4: {
+        mayor: 'Mi',
+        menor: 'Do♯',
+        trasposevrv: 'E',
+    },
+    5: {
+        mayor: 'Si',
+        menor: 'Sol♯',
+        trasposevrv: 'B',
+    },
+    6: {
+        mayor: 'Fa♯',
+        menor: 'Re♯',
+        trasposevrv: 'F#',
+    },
+    '-1': {
+        mayor: 'Fa',
+        menor: 'Re',
+        trasposevrv: 'F',
+    },
+    '-2': {
+        mayor: 'Si♭',
+        menor: 'Sol',
+        trasposevrv: 'Bb',
+    },
+    '-3': {
+        mayor: 'Mi♭',
+        menor: 'Do',
+        trasposevrv: 'Eb',
+    },
+    '-4': {
+        mayor: 'La♭',
+        menor: 'Fa',
+        trasposevrv: 'Ab',
+    },
+    '-5': {
+        mayor: 'Re♭',
+        menor: 'Si♭',
+        trasposevrv: 'Db',
+    },
+    '-6': {
+        mayor: 'Sol♭',
+        menor: 'Mi♭',
+        trasposevrv: '+Gb',
+    },
 }
