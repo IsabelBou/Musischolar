9f2aefbc8f02d487f884a45a80a9020909682504
Isabel Bou <isabelbouaguiar@gmail.com>
2024-01-17 10:13:51 +0100

    feat: Tempo is now coherent and persistent

--- src/components/ToneSelector.jsx
+++ src/components/ToneSelector.jsx
@@ -1,28 +1,25 @@
 import { Button, Tooltip } from "@chakra-ui/react";
 import { GiGClef } from "react-icons/gi";
-//import ToneWheel from "./ToneWheel";
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react'
 import ToneSlider from "./ToneSlider";
 
+// IMPROVE: Replace slider with circle of fifths wheel menu
 
-//TODO: Generate MEI file for tone selection. See: https://book.verovio.org/advanced-topics/transposition.html#transposition-by-tonic-pitch
+// TODO: Generate MEI file for tone selection. See: https://book.verovio.org/advanced-topics/transposition.html#transposition-by-tonic-pitch
+
+// IMPROVE: Have tooltip display tonality preview
 
 const ToneSelector = () => {
-
   const { isOpen, onOpen, onClose } = useDisclosure()
-  //
-
 
   return (
     <div>
-
       <Tooltip label = 'Select Tonality Pitch' >
         <Button aria-label = "Select Tonality Pitch" 
           onClick = { onOpen }>
           <GiGClef />
         </Button>
       </Tooltip>
-
       <Modal isOpen = { isOpen } onClose = { onClose }  
         size = 'sm' isCentered >
         <ModalOverlay backdropFilter='auto' backdropBlur='1.5px' backdropSaturate='50%'/>
