14993f3315ea7d54a2a2bf20ca68e0a43f611e47
Isabel Bou <isabelbouaguiar@gmail.com>
2024-06-21 15:50:57 +0200

    feat: implemented render props pattern into previous cards and updated its usage in Lenguaje Musical

--- src/components/AppShell/Header.jsx
+++ src/components/AppShell/Header.jsx
@@ -16,7 +16,7 @@ export const Header = (props) => {
             alignItems = "center"
             justifyContent = "space-between">
             <MenuButton onOpen = { onOpen } />
-            <Heading>
+            <Heading size={{ base: "md", md: "lg", lg: "xl" }}>
                 <Link as = { ReactRouterLink } to = "/">
                     Musischolar
                 </Link>
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,6 +1,5 @@
 import { CustomCard }       from "./CustomCard";
 import { ParallelCards }    from "./ParallelCard";
 import { WideCard }         from "./WideCard";
-import { CustomizableCard } from "./CustomizableCard";
 
-export { CustomCard, ParallelCards, WideCard, CustomizableCard }
+export { CustomCard, ParallelCards, WideCard }
