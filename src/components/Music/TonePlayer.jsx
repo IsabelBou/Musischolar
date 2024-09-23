deefab07a3b6809ed75d93eb44d5c4097071a514
Isabel Bou <isabelbouaguiar@gmail.com>
2024-09-23 16:17:31 +0200

    feat: Changed synth into sampler for better sound and polyphony

--- src/components/Music/TonePlayer.jsx
+++ src/components/Music/TonePlayer.jsx
@@ -1,12 +1,27 @@
 import { useEffect  } from 'react';
 import * as Tone from 'tone';
+const synth = new Tone.Sampler({
+    urls: {
+					C4: "C4.mp3",
+					"D#4": "Ds4.mp3",
+					"F#4": "Fs4.mp3",
+					A4: "A4.mp3",
+					C5: "C5.mp3",
+					"D#5": "Ds5.mp3",
+					"F#5": "Fs5.mp3",
+					A5: "A5.mp3",
+					C6: "C6.mp3",
+				},
+    release: 1,
+    baseUrl: "https://tonejs.github.io/audio/salamander/",
+}).toDestination();
 
 export const TonePlayer = (props) => {
     const { midiJson, isPlaying} = props;
-    const synth = new Tone.Synth().toDestination();
 
     useEffect( () => {
         if (isPlaying) {
+            Tone.start();
             new Tone.Sequence(((time, note) => {
                 synth.triggerAttackRelease(note, "16n", time);
             }), midiJson.tracks[0].notes.map(note => note.name), "4n").start();
@@ -16,5 +31,5 @@ export const TonePlayer = (props) => {
             Tone.Transport.cancel(0);
             Tone.Transport.stop();
         }
-    }, [isPlaying, midiJson, synth])
+    }, [isPlaying, midiJson])
 }
